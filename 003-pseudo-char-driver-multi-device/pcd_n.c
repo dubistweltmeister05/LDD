8cc5e21ffee7c921c66bc256c9daec769f73efee
ronin <kshitijvaze@gmail.com>
2025-06-25 23:22:15 +0530

    Making the PCD driver multi-device

--- 003-pseudo-char-driver-multi-device/pcd_n.c
+++ 003-pseudo-char-driver-multi-device/pcd_n.c
@@ -6,20 +6,67 @@
 #include <linux/kdev_t.h>
 #include <linux/uaccess.h>
 #include <linux/err.h>
-// this is a pseudo char device
-#define DEV_MEM_SIZE 512
-char device_buffer[DEV_MEM_SIZE];
 
 #define pr_fmt(fmt) "%s :" fmt, __func__
 
-// give it something to hold the device number in
-dev_t device_number;
+#define NO_OF_DEVICES 4
 
-// Cdev variable
-struct cdev pcd_cdev;
+// These are the memory sizes for each PCD device
+#define MEM_SIZE_MAX_PCDEV1 1024
+#define MEM_SIZE_MAX_PCDEV2 1024
+#define MEM_SIZE_MAX_PCDEV3 1024
+#define MEM_SIZE_MAX_PCDEV4 1024
 
-loff_t pcd_lseek(struct file *filp, loff_t offset, int whence)
+// These are the banks for all PCD devices
+char device_buffer_pcdev1[MEM_SIZE_MAX_PCDEV1];
+char device_buffer_pcdev2[MEM_SIZE_MAX_PCDEV2];
+char device_buffer_pcdev3[MEM_SIZE_MAX_PCDEV3];
+char device_buffer_pcdev4[MEM_SIZE_MAX_PCDEV4];
+
+/*Defining a device's private data structure*/
+struct pcdev_priv_data
 {
+    char *buffer;
+    unsigned size;
+    const char *sr_no;
+    int perm;
+    struct cdev cdev;
+};
+
+/*Defining a driver's private data structure*/
+struct pcdrv_priv_data
+{
+    int total_devices;
+    dev_t device_number;
+    struct class *class_pcd;
+    struct device *device_pcd;
+    struct pcdev_priv_data pcdev_data[NO_OF_DEVICES];
+};
+
+struct pcdrv_priv_data pcdrv_data = {
+    .total_devices = NO_OF_DEVICES,
+    .pcdev_data = {
+        [0] = {
+            .buffer = device_buffer_pcdev1,
+            .size = MEM_SIZE_MAX_PCDEV1,
+            .sr_no = "PCDEV1XYZ",
+            .perm = 0x01, /*RDONLY*/
+        },
+        [1] = {
+            .buffer = device_buffer_pcdev2, .size = MEM_SIZE_MAX_PCDEV2, .sr_no = "PCDEV2XYZ", .perm = 0x10, /*WRONLY*/
+        },
+        [2] = {
+            .buffer = device_buffer_pcdev3, .size = MEM_SIZE_MAX_PCDEV3, .sr_no = "PCDEV3XYZ", .perm = 0x11, /*RDWR*/
+        },
+        [3] = {
+            .buffer = device_buffer_pcdev4, .size = MEM_SIZE_MAX_PCDEV4, .sr_no = "PCDEV4XYZ", .perm = 0x11, /*RDWR*/
+        },
+    }};
+
+loff_t
+pcd_lseek(struct file *filp, loff_t offset, int whence)
+{
+#if 0
     loff_t temp;
 
     pr_info("lseek requested \n");
@@ -49,12 +96,13 @@ loff_t pcd_lseek(struct file *filp, loff_t offset, int whence)
     }
 
     pr_info("New value of the file position = %lld\n", filp->f_pos);
-
+#endif
     return filp->f_pos;
 }
 
 ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos)
 {
+#if 0
     pr_info("read requested for %zu bytes \n", count);
     pr_info("current file position is = %lld", *f_pos);
 
@@ -73,13 +121,14 @@ ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_p
 
     pr_info("Number of bytes read are = %zu", count);
     pr_info("updated file position is = %lld", *f_pos);
-
+#endif
     /*return the number of bytes that have been read*/
     return count;
 }
 
 ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
 {
+#if 0
     pr_info("Write requested for %zu bytes \n", count);
     pr_info("current file position is = %lld", *f_pos);
 
@@ -101,7 +150,7 @@ ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff
 
     pr_info("Number of bytes written are = %zu", count);
     pr_info("updated file position is = %lld", *f_pos);
-
+#endif
     /*return the number of bytes that have been written*/
     return count;
 }
@@ -128,56 +177,81 @@ struct file_operations pcd_fops = {
     .owner = THIS_MODULE,
 };
 
-struct class *class_pcd;
-struct device *device_pcd;
 static int __init
 pcd_module_init(void)
 {
+
     int ret;
+    int i;
     /*1. Dynamically allocate the device number*/
-    ret = alloc_chrdev_region(&device_number, 0, 1, "PCD_devices");
+    ret = alloc_chrdev_region(&pcdrv_data.device_number, 0, NO_OF_DEVICES, "PCD_devices");
     if (ret < 0)
         goto out;
-    pr_info(" Device Number <Major>:<Minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
+    for (int i = 0; i < NO_OF_DEVICES; i++)
+    {
+        pr_info(" Device Number <Major>:<Minor> = %d:%d\n", MAJOR(pcdrv_data.device_number + i), MINOR(pcdrv_data.device_number + 1));
+    }
 
-    /*2. Let's initialize the cdev structure */
-    cdev_init(&pcd_cdev, &pcd_fops);
-
-    /*3. Register the device (a cdev structure) with VFS*/
-    pcd_cdev.owner = THIS_MODULE; // necessary to prevent accidental unloading
-
-    ret = cdev_add(&pcd_cdev, device_number, 1);
-    if (ret < 0)
-        goto unreg_chrdev;
-    /*4. Create device class under /sys/class */
-    class_pcd = class_create("pcd_class");
-    if (IS_ERR(class_pcd))
+    pcdrv_data.class_pcd = class_create("pcd_class");
+    if (IS_ERR(pcdrv_data.class_pcd))
     {
         pr_info("pr error\n");
-        ret = PTR_ERR(class_pcd);
+        ret = PTR_ERR(pcdrv_data.class_pcd);
         goto cdev_del;
     }
-    /*5. Populete the sysfs with device info*/
-    device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd_devices");
-    pr_info("Module Init is successful!\n");
+
+    for (i = 0; i < NO_OF_DEVICES; i++)
+    {
+        /*2. Let's initialize the cdev structure */
+        cdev_init(&pcdrv_data.pcdev_data[i].cdev, &pcd_fops);
+
+        /*3. Register the device (a cdev structure) with VFS*/
+        pcdrv_data.pcdev_data[i].cdev.owner = THIS_MODULE; // necessary to prevent accidental unloading
+
+        ret = cdev_add(&pcdrv_data.pcdev_data[i].cdev, pcdrv_data.device_number, 1);
+        if (ret < 0)
+            goto unreg_chrdev;
+        /*4. Create device class under /sys/class */
+
+        /*5. Populete the sysfs with device info*/
+        pcdrv_data.device_pcd = device_create(pcdrv_data.class_pcd, NULL, pcdrv_data.device_number, NULL, "pcdev-%d", i);
+        if (IS_ERR(pcdrv_data.device_pcd))
+        {
+            pr_err("Device create failed\n");
+            ret = PTR_ERR(pcdrv_data.device_pcd);
+            goto class_del;
+        }
+    }
+
+    pr_info("Module init was successful\n");
+
     return 0;
 
 cdev_del:
-    cdev_del(&pcd_cdev);
+class_del:
+    for (; i >= 0; i--)
+    {
+        device_destroy(pcdrv_data.class_pcd, pcdrv_data.device_number + i);
+        cdev_del(&pcdrv_data.pcdev_data[i].cdev);
+    }
+    class_destroy(pcdrv_data.class_pcd);
 
 unreg_chrdev:
-    unregister_chrdev_region(device_number, 1);
+    unregister_chrdev_region(pcdrv_data.device_number, NO_OF_DEVICES);
 out:
+    pr_info("Module insertion failed\n");
     return ret;
 }
 
 static void __exit pcd_driver_cleanup(void)
 {
+#if 0
     device_destroy(class_pcd, device_number);
     class_destroy(class_pcd);
     cdev_del(&pcd_cdev);
     unregister_chrdev_region(device_number, 1);
     pr_info("module unloaded\n");
+#endif
 }
 
 module_init(pcd_module_init);
@@ -185,5 +259,5 @@ module_exit(pcd_driver_cleanup);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("RONIN");
-MODULE_DESCRIPTION("A SIMPLE HELLO-WRLD MODULE TO TEST THE WATERS");
+MODULE_DESCRIPTION("A PSEUDO-CHAR DRIVER TO HANDLE MULTIPLE CHAR DEVICES");
 MODULE_INFO(board, "BeagleBone Black Rev C");
